985bf572216169eaf1f8d6ed04f33f8417dcb593
Peter Hannon <46349689+petehannon@users.noreply.github.com>
2022-03-03 01:36:09 +0000

    Fix formating of ruleset

--- src/rules.mk
+++ src/rules.mk
@@ -5,6 +5,6 @@ OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 VIA_ENABLE      = no
-WPM_ENABLE		= yes
+WPM_ENABLE      = yes
 SRC += oled/oled-bongocat.c oled/oled-icons.c
 OPT_DEFS += -DLEFTCAT
